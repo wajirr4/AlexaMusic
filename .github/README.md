3364354d2411979956cd6c34e2ca7d1b8e4b2827
Dr Asad Ali <asadabbasshah2@gmail.com>
2022-06-12 01:48:45 +0500

    Alexa Music 3.0

---
+++ .github/README.md
@@ -0,0 +1,14 @@
+## 🚀 Heroku Deployment
+
+<h4>Click the button below to deploy Ramdi on Heroku!</h4>    
+<a href="https://dashboard.heroku.com/new?template=https://github.com/Jankarikiduniya/AlexaMusic"><img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200""/></a>
+
+
+
+
+.
+
+
+
+
+[![Deploy+To+Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https://github.com/Jankarikiduniya/AlexaMusic&envs=STRING_SESSION,BOT_TOKEN,MUSIC_BOT_NAME,API_ID,API_HASH,OWNER_ID,DURATION_LIMIT,AUTO_LEAVING_ASSISTANT,MONGO_DB_URI,START_IMG_URL,PING_IMG_URL,OWNER_ID,UPSTREAM_BRANCH,UPSTREAM_REPO,LOG_GROUP_ID,SUPPORT_CHANNEL,SUPPORT_GROUP,GIT_TOKEN)
---
+++ AlexaMusic/__main__.py
@@ -0,0 +1,88 @@
+#
+# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali 
+# Harshit Sharma
+# All rights reserved. © Alisha © Alexa © Yukki
+
+
+
+
+
+
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from AlexaMusic import LOGGER, app, userbot
+from AlexaMusic.core.call import Yukki
+from AlexaMusic.plugins import ALL_MODULES
+from AlexaMusic.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("AlexaMusic").error(
+            "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("AlexaMusic").warning(
+            "Spotify Client Id & Secret not added so your bot won't be able to play spotify tracks and playlists, Chutiya Saala ek itni simple cheej nahi laa paaya."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("AlexaMusic.plugins" + all_module)
+    LOGGER("AlexaMusic.plugins").info(
+        "Necessary Modules Imported Successfully."
+    )
+    await userbot.start()
+    await Yukki.start()
+    try:
+        await Yukki.stream_call(
+            "https://telegra.ph/file/162cdb7ebf049ec086bae.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("AlexaMusic").error(
+            "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
+        )
+        sys.exit()
+    except:
+        pass
+    await Yukki.decorators()
+    LOGGER("AlexaMusic").info("Music Bot Started Successfully, Now Gib Party to @cute_boy701")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("AlexaMusic").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
