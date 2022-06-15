fb55895b40eb1322290c7f84563f87612e883c37
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-06-15 17:19:31 +0500

    Alexa Music 3.0

--- AlexaMusic/plugins/sudo/SudoUsers.py
+++ AlexaMusic/plugins/sudo/SudoUsers.py
@@ -1,4 +1,4 @@
-usic Bot Property Of Rocks Indian Largest Chatting Group
+# Music Bot Property Of Rocks Indian Largest Chatting Group
 # Without Credit (Mother Fucker)
 # Rocks © @Dr_Asad_Ali © Rocks
 # Owner Asad Ali
