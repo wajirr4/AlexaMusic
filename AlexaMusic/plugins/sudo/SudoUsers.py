798ac95815ddb263060d28338906bdc51a96b51f
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-06-15 17:27:44 +0500

    Alexa Music 3.0

--- AlexaMusic/plugins/sudo/SudoUsers.py
+++ AlexaMusic/plugins/sudo/SudoUsers.py
@@ -1,4 +1,4 @@
-# Music Bot Property Of Rocks Indian Largest Chatting Group
+usic Bot Property Of Rocks Indian Largest Chatting Group
 # Without Credit (Mother Fucker)
 # Rocks © @Dr_Asad_Ali © Rocks
 # Owner Asad Ali
@@ -15,8 +15,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-from config import OWNER_ID
-from AlexaMusic import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, OWNER_ID, SUDOERS, app
+from config import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, OWNER_ID, SUDOERS, app
 from AlexaMusic.utils.database import (add_gban_user, get_served_chats, get_sudoers,
                             is_gbanned_user, remove_gban_user)
 
