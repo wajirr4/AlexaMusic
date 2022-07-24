694fa2b0f11844ab2b73905d6ebbd3ea761a09f1
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-07-24 16:36:55 +0500

    Alexa Music

--- AlexaMusic/plugins/tools/speedtest.py
+++ AlexaMusic/plugins/tools/speedtest.py
@@ -13,8 +13,8 @@ import asyncio
 import speedtest
 from pyrogram import filters
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from AlexaMusic import app
+from AlexaMusic.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
